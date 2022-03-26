5a48744a1b8c1b51eefd993b8653bd45cb3b15f3
Ben Black <weepingwillowben@gmail.com>
2022-03-25 22:06:12 -0600

    finished chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
+
+
+    loop{
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("You guessed: {}", guess);
+        
+        let int_guess = match guess.trim().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Guess is not an integer, try again.");
+                continue;
+            },
+        };
+        
+        match int_guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal =>  {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
