cb0a1928fd530da6bb34a804d5a79680ed8fee5b
Ben Black <weepingwillowben@gmail.com>
2022-05-01 14:24:10 -0600

    added gold test

--- hashlife-algo/src/lib.rs
+++ hashlife-algo/src/lib.rs
@@ -14,6 +14,82 @@ mod tests {
     use super::*;    
     use std::fs;
  
+    fn life_forward_fn(
+        sum:u8,
+        curval:u8
+    ) -> u8 {
+        if sum == 3 {
+            1
+        }
+        else if sum == 4 {
+            curval
+        }
+        else{
+            0
+        }
+    }
+    
+    fn step_forward_automata(prevmap: &[u8], nextmap: &mut [u8], xsize:usize, ysize: usize){
+        for y in 1..(ysize-1){
+            let ymaps = [
+                &prevmap[((y-1)*xsize)..((y+0)*xsize)],
+                &prevmap[((y+0)*xsize)..((y+1)*xsize)],
+                &prevmap[((y+1)*xsize)..((y+2)*xsize)],
+            ];
+            //sums the first elements
+            for x in 1..(xsize-1){
+                let csum:u8 = ymaps.iter().map(|v|{let sr:u8 = v[(x-1)..(x+1+1)].iter().sum();sr}).sum();
+                let nextval = life_forward_fn(csum, ymaps[1][x]);
+                nextmap[y*xsize+x] = nextval;
+            }
+        }
+    }
+    fn set_map(points: &Vec<Point>, map: &mut[u8], xsize:usize, ysize: usize){
+        for p in points.iter(){
+            map[(p.y as usize)*xsize+p.x as usize] = 1;
+        }
+    }
+    fn map_to_points(map: &[u8], xsize:usize, ysize: usize)->Vec<Point>{
+        let mut res: Vec<Point> = Vec::new();
+        for y in 0..ysize{
+            for x in 0..xsize{
+                if map[y*xsize+x] != 0{
+                    res.push(Point{
+                        x:x as i64,
+                        y:y as i64,
+                    });
+                }
+            }
+        }
+        res
+    }
+    fn points_equal(v1: &Vec<Point>, v2: &Vec<Point>)->bool{
+        let mut v1s = v1.clone();
+        let mut v2s = v2.clone();
+        v1s.sort();
+        v2s.sort();
+        v1s.len() == v2s.len() && v1s.iter().zip(v2s.iter()).all(|(x1,x2)|x1 == x2)
+    }
+    fn step_forward_gold(points: &Vec<Point>, n_steps: u64)->Vec<Point>{
+        const SIZE: usize = 1000;
+        let centered_points = points.iter().map(|x|*x+Point{x:(SIZE/2) as i64,y:(SIZE/2) as i64}).collect();
+        let mut map = vec![0 as u8; SIZE*SIZE];
+        set_map(&centered_points, &mut map, SIZE, SIZE);
+
+        for _ in 0..n_steps{
+            let mut newmap = vec![0 as u8; SIZE*SIZE];
+            step_forward_automata(&map, &mut newmap, SIZE, SIZE);
+            map.clone_from_slice(&newmap);
+        }
+        // let mut mutdata
+        map_to_points(&map, SIZE, SIZE)
+    }
+    fn step_forward_actual(points: &Vec<Point>, n_steps: u64)->Vec<Point>{
+        let mut tree = TreeData::gather_all_points(&points);
+        tree.step_forward(n_steps);
+        tree.dump_all_points()
+    }
+    
     fn dump_points_to_str(points: &Vec<Point>)->String{
         let mut sorted_points = points.clone();
         let minx = sorted_points.iter().map(|p|p.x).min().unwrap();
@@ -74,5 +150,23 @@ mod tests {
     
         let rle_tot_str = write_rle(&out_points);
         assert_eq!(expected, rle_tot_str);
+    }
+    
+    #[test]
+    fn test_quadtree_against_gold() {
+        let contents = concat!(
+            "x = 12, y = 8, rule = B3/S23\n",
+            "12bo8bo$bo2bo2b2o2bo25bo2b2o2bo2bo$6bo5bo7b3o3b3o7bo5bo$6bo5bo8bo5bo8bo5bo$6bo5bo8b7o8bo5bo$bo2bo2b2o2bo2b2o4bo7bo4b2o2bo2b2o2bo2bo$o8bo3b2o4b11o4b2o3bo8bo$o3bo9b2o17b2o9bo3bo$4o11b19o11b4o$16bobo11bobo$19b11o$19bo9bo$20b9o$24bo$20b3o3b3o$22bo3bo$$21b3ob3o$21b3ob3o$20bob2ob2obo$20b3o3b3o$21bo5bo!\n"
+        );
+        let points = parse_fle_file(contents);
+        let n_steps = 1;
+        let gold_points = step_forward_gold(&points, n_steps);
+        let actual_points = step_forward_actual(&points, n_steps);
+        fs::write("gold_points.txt", dump_points_to_str(&gold_points))
+           .expect("failed to open points.txt file for writing");
+       fs::write("actual_points.txt", dump_points_to_str(&actual_points))
+           .expect("failed to open points.txt file for writing");
+   
+        assert!(points_equal(&gold_points, &actual_points));
     }
 }
